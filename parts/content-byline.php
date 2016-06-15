1c7250aa81cbb756e2e91405790451cfd7ade88a
Daniel Plinsinga <info@toastdesign.nl>
2016-06-15 15:49:55 +0200

    Translatable byline

--- parts/content-byline.php
+++ parts/content-byline.php
@@ -1,3 +1,3 @@
-<p class="byline">
-	Posted on <?php the_time('F j, Y') ?> by <?php the_author_posts_link(); ?>  - <?php the_category(', ') ?>
-</p>	
+<p class="byline">  
+	<?php printf( __( 'Geplaatst op %1$s, door %2$s', 'my-text-domain' ), $time = get_the_time('F j Y'), $author = get_the_author_posts_link() ); ?>
+</p>
