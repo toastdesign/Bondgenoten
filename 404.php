927d3d515e7e22d23170b8ce38cd7ceb25bf7fcc
Daniel Plinsinga <info@toastdesign.nl>
2016-06-15 15:55:13 +0200

    Enekel vertalingen

--- 404.php
+++ 404.php
@@ -9,11 +9,11 @@
 				<article id="content-not-found">
 				
 					<header class="article-header">
-						<h1><?php _e( 'Epic 404 - Article Not Found', 'jointswp' ); ?></h1>
+						<h1><?php _e( 'Epic 404 - Artikel niet gevonden', 'jointswp' ); ?></h1>
 					</header> <!-- end article header -->
 			
 					<section class="entry-content">
-						<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
+						<p><?php _e( 'Helaas, maar de opgevraagde pagina kon niet worden gevonden. Misschien kan de zoekfunctie helpen!', 'jointswp' ); ?></p>
 					</section> <!-- end article section -->
 
 					<section class="search">
