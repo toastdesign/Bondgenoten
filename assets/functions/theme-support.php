acfd8a57c13d7c57d67044da466856b023f7c268
Daniel Plinsinga <info@toastdesign.nl>
2016-06-15 17:20:52 +0200

    Disabled posts format

--- assets/functions/theme-support.php
+++ assets/functions/theme-support.php
@@ -24,18 +24,18 @@ function joints_theme_support() {
 	         ) 
 	);
 	
-	// Adding post format support
+	// Adding post format support (Just uncomment what we need)
 	 add_theme_support( 'post-formats',
 		array(
-			'aside',             // title less blurb
-			'gallery',           // gallery of images
-			'link',              // quick link to other site
-			'image',             // an image
-			'quote',             // a quick quote
-			'status',            // a Facebook like status update
-			'video',             // video
-			'audio',             // audio
-			'chat'               // chat transcript
+			// 'aside',             // title less blurb
+			// 'gallery',           // gallery of images
+			// 'link',              // quick link to other site
+			// 'image',             // an image
+			// 'quote',             // a quick quote
+			// 'status',            // a Facebook like status update
+			// 'video',             // video
+			// 'audio',             // audio
+			// 'chat'               // chat transcript
 		)
 	); 
 	
